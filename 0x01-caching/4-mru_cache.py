00ab32eccdecaa3214ec39e3fdbac7931e3a5b77
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-07-08 03:44:33 +0100

    new implementation

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -6,35 +6,59 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """A class for LRU caching system
+    """A class for MRU (Most Recently Used) caching system.
+
+    Inherits from BaseCaching and implements MRU eviction strategy
+    when the cache reaches its maximum capacity.
 
     Args:
-        BaseCaching (Base Class for caching):
-        parent class template for caching
+        BaseCaching (class): Parent class template for caching.
+
+    Attributes:
+        access_order (list): Keeps track of the access order of keys.
+
+    Methods:
+        __init__():
+            Initializes the MRUCache instance.
+
+        put(key, item):
+            Adds or updates an item in the cache.
+            If the cache is full, evicts the most recently used item.
+
+        get(key):
+            Retrieves an item from the cache based on the specified key.
+            Updates the access order to mark the item as most recently used.
     """
+
     def __init__(self):
-        """Constructor initialization
-        """
+        """Initializes the MRUCache instance."""
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.access_order = []  # Track access order of keys
 
     def put(self, key, item):
-        """Updates the cache with the given key and item.
+        """Adds or updates an item in the cache.
+
+        If the cache is full (reaches BaseCaching.MAX_ITEMS),
+        removes the most recently used item based on access order.
 
         Args:
-            key (str): The key to add to the cache.
-            item (any): The item to add to the cache.
+            key (str): The key to add or update in the cache.
+            item (any): The item to add or update in the cache.
         """
         if key is not None and item is not None:
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                # Evict the most recently used item
-                mru_key = next(reversed(list(self.cache_data.items())))
-                print(f"DISCARD: {mru_key[0]}")  # Print key discarded
-                del self.cache_data[mru_key[0]]  # Remove the most recent item
-            self.cache_data[key] = item  # Add or update the item in the cache
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Remove the most recently used item
+                mru_key = self.access_order.pop()  # Get the last accessed key
+                del self.cache_data[mru_key]  # Remove from cache
+                print("DISCARD: {}".format(mru_key))  # Print discarded key
+
+            self.cache_data[key] = item  # Add or update item in cache
+            self.access_order.append(key)  # Update access order
 
     def get(self, key):
-        """Returns an item from the cache based on the specified key.
+        """Retrieves an item from the cache based on the specified key.
+
+        Updates the access order to mark the item as most recently used.
 
         Args:
             key (str): The key to retrieve from the cache.
@@ -44,7 +68,8 @@ class MRUCache(BaseCaching):
             or None if the key is not found.
         """
         if key is not None and key in self.cache_data:
-            item = self.cache_data[key]
-            # Accessing the item makes it the most recently used
-            return item
+            # Update access order: Move key to the end (most recent)
+            self.access_order.remove(key)
+            self.access_order.append(key)
+            return self.cache_data[key]
         return None
